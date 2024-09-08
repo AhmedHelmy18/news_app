2bf110e43fdfd952d55724e0c329950d06955037
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2024-09-08 23:00:48 +0300

    fixed the ui of app

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -6,22 +6,26 @@ class NewsService {
   final Dio dio = Dio();
 
   Future<List<ArticleModel>> getNews() async {
-    Response response = await dio.get(
-        'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=869ec58d50c84ec0a710cc1064ee1dc3');
-    Map<String, dynamic> jsonData = response.data;
+    try {
+      Response response = await dio.get(
+          'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=869ec58d50c84ec0a710cc1064ee1dc3');
+      Map<String, dynamic> jsonData = response.data;
 
-    List<dynamic> articles = jsonData['articles'];
+      List<dynamic> articles = jsonData['articles'];
 
-    List<ArticleModel> articlesList = [];
+      List<ArticleModel> articlesList = [];
 
-    for (var article in articles) {
-      ArticleModel articleModel = ArticleModel(
-        image: article['urlToImage'],
-        title: article['title'],
-        subTitle: article['description'],
-      );
-      articlesList.add(articleModel);
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel(
+          image: article['urlToImage'],
+          title: article['title'],
+          subTitle: article['description'],
+        );
+        articlesList.add(articleModel);
+      }
+      return articlesList;
+    } catch (e) {
+      return [];
     }
-    return articlesList;
   }
 }
--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -5,44 +5,33 @@ import '../models/article_model.dart';
 import '../services/news_service.dart';
 import 'news_list_view.dart';
 
-class NewsListViewBuilder extends StatefulWidget {
-  const NewsListViewBuilder({
-    super.key,
-  });
-
-  @override
-  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
-}
-
-class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
-  List<ArticleModel> articles = [];
-
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    getGeneralNews();
-  }
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsService().getNews();
-    isLoading = false;
-    setState(() {});
-  }
-
+class NewsListViewBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? SliverToBoxAdapter(
-            child: Center(
-              child: CircularProgressIndicator(
-                color: Colors.blue,
-              ),
-            ),
-          )
-        : NewsListView(
-            articles: articles,
-          );
+    return FutureBuilder(
+      builder: (context, snapshot) {
+        return NewsListView(
+          articles: snapshot.data ?? [],
+        );
+      },
+      future: NewsService().getNews(),
+    );
+    // return isLoading
+    //     ? SliverFillRemaining(
+    //         hasScrollBody: false,
+    //         child: Center(
+    //           child: CircularProgressIndicator(
+    //             color: Colors.blue,
+    //           ),
+    //         ),
+    //       )
+    //     : articles.isNotEmpty
+    //         ? NewsListView(
+    //             articles: articles,
+    //           )
+    //         : SliverFillRemaining(
+    //             hasScrollBody: false,
+    //             child: Text('oops there was an error, try later'),
+    //           );
   }
 }
