c5bde5946109be1b5cf5eddde497804bf1309cb1
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2024-09-09 20:53:50 +0300

    add factory constractor

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -16,11 +16,7 @@ class NewsService {
       List<ArticleModel> articlesList = [];
 
       for (var article in articles) {
-        ArticleModel articleModel = ArticleModel(
-          image: article['urlToImage'],
-          title: article['title'],
-          subTitle: article['description'],
-        );
+        ArticleModel articleModel = ArticleModel.fromJson(article);
         articlesList.add(articleModel);
       }
       return articlesList;
